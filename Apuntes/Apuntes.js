7d79a6fe6788b080ec2de2a1a82538e69f3f1d99
unknown <alejandrojzeballos@gmail.com>
2020-08-28 22:05:33 -0400

    añadiendo la introduccion a phaser

---
+++ Apuntes/Apuntes.js
@@ -0,0 +1,10 @@
+/* 
+Phaser es una libreria que nos permite añadir animaciones mecanicas movimientos.
+como funciona:
+un codigo js hace funcionar una pagina html
+para hacer funcionar necesitamos los recursos elemenos graficos que animar, Imagenes y adicionalmente a eso necesitamos un motor el cual es phaser con eso lograremos que exuista gravedad o colisiones. 
+descargamos la libreria de:
+https://phaser.io/download/stable
+descargamos el archivo js
+
+*/
