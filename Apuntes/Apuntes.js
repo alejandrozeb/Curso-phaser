c9b504ff8060e77d0e753cdf3c5cc344088ebb92
unknown <alejandrojzeballos@gmail.com>
2020-08-28 23:37:03 -0400

    agregando el inicio del proyecto phaser

--- Apuntes/Apuntes.js
+++ Apuntes/Apuntes.js
@@ -6,5 +6,20 @@ para hacer funcionar necesitamos los recursos elemenos graficos que animar, Imag
 descargamos la libreria de:
 https://phaser.io/download/stable
 descargamos el archivo js
-
+por consola:
+creamos una carpeta donde se alojara nuestro juego y abrimos una consola y ejecutamos el siguientes comando:
+npm init
+y llenamos los datos necessario (solo puedes llenar el nombre y saltar los demas espacios), recuerda que debes tener node instala, si deseas instalar node puedes visitar el repositorio de curso de javascript para seguir los pasos de instalacion.
+luego instalamos phaser
+npm install --save phaser
+instalra la ultima version estable de phaser.
+pero en este curso usaremos la version:
+npm install --save phaser@3.8.0
+los juegos se dividen en estados 
+el estado de menu principal sera el estado 1
+y el siguiente ya sera el juego estado 2
+el estado es cuando termine el juego estado 3
+update function
+time y delta son importantes por crean un loop infinito
+el delta se usa para que el movimientos ea el mismo en cualquier navegador
 */
---
+++ juego2/src/init.js
@@ -0,0 +1,18 @@
+const config = {
+    width:320,
+    height: 180,
+    parent: "container",
+    type: Phaser.AUTO,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+}
+var game = new Phaser.Game(config);
+
+function preload(){console.log('soy preload')};
+function create(){console.log('soy create')};
+function update(time, delta){  //actualiza el estado cada vez y recibe dos parametros el time,delta
+    console.log(delta); //muestra el delta y sus iteraciones
+};
