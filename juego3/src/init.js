5eceaabf84b9230f5707ac2de6090b8e9eb83d1c
unknown <alejandrojzeballos@gmail.com>
2020-08-29 00:23:36 -0400

    Mostrando funciones para modificar el bird del objeto pajaro

--- juego3/src/init.js
+++ juego3/src/init.js
@@ -20,8 +20,18 @@ function create(){
     //console.log('soy create')
     //esta es una escena
     //pasamos las imagen    this.add.image(x,y,nombre)
-    this.add.image(50,100,"pajaro")
+    //para controlar el pajaro lo guardamos en una variable
+    this.pajaro=this.add.image(80,100,"pajaro");
+    //mostrando todos los atributos del objeto pajaro, para poder modificar sus atributos usamos get a sets del prototype
+    console.log(this.pajaro);
+    this.pajaro.setScale(2);
+    //para girar el pajaro
+    this.pajaro.flipX=true;
+    //para rotar el pajaro
+    //this.pajaro.setRotation(30);
+    //en grados
+    this.pajaro.setAngle(30);
 }
 function update(time, delta){  //actualiza el estado cada vez y recibe dos parametros el time,delta
-    console.log(delta); //muestra el delta y sus iteraciones
+    //console.log(delta); //muestra el delta y sus iteraciones
 };
