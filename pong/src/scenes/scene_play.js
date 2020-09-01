bbdf5cf8a6e1047f834c14cc21b5103537a0e80e
unknown <alejandrojzeballos@gmail.com>
2020-09-01 00:08:26 -0400

    añadiendo funcionalidad a la bola

--- pong/src/scenes/scene_play.js
+++ pong/src/scenes/scene_play.js
@@ -37,11 +37,22 @@ class Scene_play extends Phaser.Scene{
         //cuando choquen tiene que ejecutar una funcion
        this.physics.add.collider(this.ball, this.izquierda,this.chocaPala,null,this);
        this.physics.add.collider(this.ball, this.derecha,this.chocaPala,null,this);
-
+        //controles
+        //derecha
+        this.cursor = this.input.keyboard.createCursorKeys();
+        //izquierda
+        this.cursor_W = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        this.cursor_S = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
     }
-
+    update(){
+        //cada vez que sale ingesa por centro
+        if(this.ball.x <0 || this.ball.x > this.sys.game.config.width ){
+            this.ball.setPosition(this.sys.game.config.width/2,this.sys.game.config.height/2);
+        }
+    }
     chocaPala(){
-
+        //da un aleatorio en y 
+        this.ball.setVelocityY(Phaser.Math.Between(-120,120));
     }
 }
 export default Scene_play;
